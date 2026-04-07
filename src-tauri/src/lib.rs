2c4d5365977e09eb9611117264d7794dd6413af5
LLergibt <gosha2007naz.com>
2026-04-07 05:50:34 -0700

    feat: installed plugin for working with projects directories

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,6 +12,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .manage(WorkspaceService::new())
         .invoke_handler(tauri::generate_handler![
             greet,
